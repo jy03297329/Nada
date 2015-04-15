5510dcd4194367f546204ed5c9deeafcdd516db3
Yuanzheng Chang <hjcyz1991@gmail.com>
2015-04-15 15:34:12 -0500

    first commit

---
+++ app/src/androidTest/java/com/example/hjcyz1991/project407/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.hjcyz1991.project407;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
