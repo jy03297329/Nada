60f31f061a5394213abfa429f6d01b7a4a289993
Winston Yan <Winston@dyn-72-33-59-64.uwnet.wisc.edu>
2015-05-10 23:00:21 -0500

    final

--- app/src/main/java/com/example/hjcyz1991/project407/PayBack.java
+++ app/src/main/java/com/example/hjcyz1991/project407/PayBack.java
@@ -2,10 +2,12 @@ package com.example.hjcyz1991.project407;
 
 import android.app.FragmentManager;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -18,7 +20,9 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.hjcyz1991.project407.Model.Backend;
 import com.example.hjcyz1991.project407.Model.Bill;
+import com.example.hjcyz1991.project407.Model.BillEvent;
 import com.example.hjcyz1991.project407.Model.User;
 import com.hannesdorfmann.swipeback.Position;
 import com.hannesdorfmann.swipeback.SwipeBack;
@@ -36,6 +40,7 @@ public class PayBack extends ActionBarActivity implements PaymentMethodDialog.Co
     private User user;
     private List<Bill> billList;
     private int position;
+    private SettleTask mSettle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,11 +48,16 @@ public class PayBack extends ActionBarActivity implements PaymentMethodDialog.Co
         SwipeBack.attach(this, Position.LEFT)
                 .setContentView(R.layout.activity_pay_back)
                 .setSwipeBackView(R.layout.swipeback_default);
+        String userBackendID = SaveSharedPreference.getUserName(getApplicationContext());
+        //Log.d("MAIN_ACTIVITY", "getting user ID from login activity: " + userBackendID);
+        List<User> users = User.find(User.class, "backend_id = ?", userBackendID);
+        user = users.get(0);
 //        payback_search = (EditText)findViewById(R.id.pay_back_search);
 //        pay = (Button)findViewById(R.id.button_pay);
 //        infoView = (TextView)findViewById(R.id.pay_back_info);
         tableLayout = (TableLayout)findViewById(R.id.pay_back_row_container);
         billList = user.getBillPay();
+
         for(int i = 0; i < billList.size(); i ++){
             position = i;
             TableRow rowView = new TableRow(this);
@@ -57,10 +67,13 @@ public class PayBack extends ActionBarActivity implements PaymentMethodDialog.Co
             textView.setText(billList.get(i).toString(user.backendId));
             Button payButton = new Button(this);
             payButton.setLayoutParams(new TableRow.LayoutParams((TableRow.LayoutParams.WRAP_CONTENT), TableRow.LayoutParams.WRAP_CONTENT, 1f));
+            final String BID = Integer.toString(billList.get(i).backendId);
             payButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     showDialog(v);
+                    mSettle = new SettleTask(BID);
+                    mSettle.execute();
                 }
             });
             rowView.addView(textView,0);
@@ -114,6 +127,50 @@ public class PayBack extends ActionBarActivity implements PaymentMethodDialog.Co
     @Override
     public void onDialogMessage(String message) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    public class SettleTask extends AsyncTask<Void, Void, Boolean> {
+
+        private String billId;
+        SettleTask(String b) {
+            billId = b;
+            Log.d(null, "created a new EventTask");
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            //while(!isCancelled()){
+            final String TAG = "LOGIN_ACTIVITY_EVENT";
+            Log.d(TAG, "EventTask in");
+            Backend.settleBill(Integer.toString(user.backendId), user.authToken, billId,
+                    new Backend.BackendCallback() {
+                        @Override
+                        public void onRequestCompleted(Object result) {
+                            //Log.d(null, "5");
+
+                            Bill setBill = (Bill)result;
+                            int bId = setBill.backendId;
+                            List<Bill> bills = Bill.find(Bill.class, "backend_id = ?", Integer.toString(bId));
+                            bills.get(0).delete();
+
+                        }
+
+                        @Override
+                        public void onRequestFailed(final String message) {
+
+                            Log.d(TAG, "Received error from Backend: " + message);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        }
+                    });
+            //while(loadLock.isLocked()){};
+            return true;
+        }
     }
 
 
